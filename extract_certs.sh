e6f5d1934542ede367698e015d1780d54446b849
d43v30n <anjey.mail@gmail.com>
2024-10-15 10:56:18 +0200

    upd file path

--- extract_certs.sh
+++ extract_certs.sh
@@ -4,7 +4,7 @@
 source .env
 
 # Use the DOMAIN variable from the .env file in the jq command
-jq -r ".myresolver.Certificates[] | select(.domain.main==\"$DOMAIN\") | .certificate" ./letsencrypt/acme.json | base64 -d > fullchain.pem
-jq -r ".myresolver.Certificates[] | select(.domain.main==\"$DOMAIN\") | .key" ./letsencrypt/acme.json | base64 -d > privkey.pem
+jq -r ".myresolver.Certificates[] | select(.domain.main==\"$DOMAIN\") | .certificate" ./letsencrypt/acme.json | base64 -d > ./maddy_tls/fullchain.pem
+jq -r ".myresolver.Certificates[] | select(.domain.main==\"$DOMAIN\") | .key" ./letsencrypt/acme.json | base64 -d > ./maddy_tls/privkey.pem
 
 echo "Certificates extracted successfully for domain: $DOMAIN"
